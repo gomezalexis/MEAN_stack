ea49d2496486c96d37d44f596eed1ee6f7908cbe
alexis gomez <gomez.alexis@gmail.com>
2017-10-04 21:02:47 -0500

    Math Module

---
+++ Node/mathModule/app.js
@@ -0,0 +1,7 @@
+
+var my_module = require('./mathlib');
+
+console.log(my_module.add(2,3));
+console.log(my_module.multiply(3,5));
+console.log(my_module.square(5));
+console.log(my_module.random(2,36));
